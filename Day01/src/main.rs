321a1b2f558ad2f469fbada964688210f074bf91
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-02 09:54:51 +1300

    Part 1 test passes

--- Day01/src/main.rs
+++ Day01/src/main.rs
@@ -4,16 +4,38 @@ fn main() {
 
 #[test]
 fn example_input_test() {
-
     assert_eq!(
-        solve(r#"
-            1abc2
-            pqr3stu8vwx
-            a1b2c3d4e5f
-            treb7uchet
-        "#), 142);
+        solve(
+            "1abc2\n\
+            pqr3stu8vwx\n\
+            a1b2c3d4e5f\n\
+            treb7uchet"
+        ),
+        142
+    );
 }
 
 fn solve(input: &str) -> u32 {
-    return 0
+    let mut first_num = ' ';
+    let mut last_num = ' ';
+    let mut result = 0;
+    for (_, a) in input.chars().enumerate() {
+        if a == '\n' {
+            let number = format!("{}{}", first_num, last_num);
+            result += number.parse::<u32>().unwrap();
+            first_num = ' ';
+            last_num = ' ';
+        }
+        if a.is_numeric() {
+            last_num = a;
+            if first_num == ' ' {
+                first_num = a;
+            }
+        }
+    }
+
+    let number = format!("{}{}", first_num, last_num);
+    result += number.parse::<u32>().unwrap();
+
+    result
 }
