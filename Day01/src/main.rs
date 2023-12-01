616df7a6a522c676d66aa807cc59bb3c28491f78
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-02 08:57:02 +1300

    Create unti test with example input

--- Day01/src/main.rs
+++ Day01/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn example_input_test() {
+
+    assert_eq!(
+        solve(r#"
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        "#), 142);
+}
+
+fn solve(input: &str) -> u32 {
+    return 0
+}
