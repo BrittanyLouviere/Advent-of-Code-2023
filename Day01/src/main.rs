c650a557c80529435944a5ffcc574df55fe92838
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-02 10:18:54 +1300

    Setup for part 2
    Added part 2 unit test and solution function
    Changed input to be borrowed

--- Day01/src/main.rs
+++ Day01/src/main.rs
@@ -2,14 +2,15 @@ use std::fs;
 
 fn main() {
     let input: String = fs::read_to_string("./input.txt").unwrap();
-    println!("{}", solve(input));
+    println!("Part 1: {}", solve_part_1(&input));
+    println!("Part 2: {}", solve_part_2(&input));
 }
 
 #[test]
-fn example_input_test() {
+fn example_input_part_1_test() {
     assert_eq!(
-        solve(
-            "1abc2\n\
+        solve_part_1(
+            &"1abc2\n\
             pqr3stu8vwx\n\
             a1b2c3d4e5f\n\
             treb7uchet"
@@ -19,7 +20,7 @@ fn example_input_test() {
     );
 }
 
-fn solve(input: String) -> u32 {
+fn solve_part_1(input: &String) -> u32 {
     let mut first_num = ' ';
     let mut last_num = ' ';
     let mut result = 0;
@@ -42,4 +43,25 @@ fn solve(input: String) -> u32 {
     result += number.parse::<u32>().unwrap();
 
     result
+}
+
+#[test]
+fn example_input_part_2_test() {
+    assert_eq!(
+        solve_part_2(
+            &"two1nine\n\
+            eightwothree\n\
+            abcone2threexyz\n\
+            xtwone3four\n\
+            4nineeightseven2\n\
+            zoneight234\n\
+            7pqrstsixteen"
+                .to_string()
+        ),
+        281
+    );
+}
+
+fn solve_part_2(input: &String) -> u32 {
+    0
 }
