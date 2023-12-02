8b998d1e9fc5b128d856f00a1586e830633e3db6
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-03 11:33:42 +1300

    Fill in part 2 unit test

--- Day02/src/main.rs
+++ Day02/src/main.rs
@@ -12,10 +12,10 @@ fn example_input_part_1_test() {
     assert_eq!(
         solve_part_1(
             "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
-        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
-        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
-        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
-        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
         ),
         8
     );
@@ -50,9 +50,20 @@ fn solve_part_1(input: &str) -> u32 {
 
 #[test]
 fn example_input_part_2_test() {
-    assert_eq!(solve_part_2(&"".to_string()), 0);
+    assert_eq!(
+        solve_part_2(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        ),
+        2286
+    );
 }
 
 fn solve_part_2(_input: &str) -> u32 {
+    // power = #red * #green #blue
+    // result = sum of power of each game
     0
 }
