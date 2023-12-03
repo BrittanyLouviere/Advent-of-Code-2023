a1ca012e79f8603fefe78f575fb9234d518bf1dd
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-04 10:29:19 +1300

    Moved part functions into separate modules

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,126 +3,130 @@ use std::fs;
 
 fn main() {
     let input: String = fs::read_to_string("./day03/input.txt").unwrap();
-    println!("Part 1: {}", solve_part_1(&input));
-    println!("Part 2: {}", solve_part_2(&input));
+    println!("Part 1: {}", part_1::solve(&input));
+    println!("Part 2: {}", part_2::solve(&input));
 }
 
-fn solve_part_1(input: &str) -> u32 {
-    // sum all numbers adjacent to a symbol
-    let mut sum = 0;
+mod part_1 {
+    pub(crate) fn solve(input: &str) -> u32 {
+        // sum all numbers adjacent to a symbol
+        let mut sum = 0;
 
-    let lines: Vec<&str> = input.lines().collect();
-    let padding_line = &".".repeat(lines[0].len());
-    for i in 0..lines.len() {
-        let prev = format!(".{}.", if i == 0 { padding_line } else { lines[i - 1] });
-        let curr = format!(".{}.", lines[i]);
-        let next = format!(
-            ".{}.",
-            if i == lines.len() - 1 {
-                padding_line
-            } else {
-                lines[i + 1]
-            }
-        );
-        sum += check_line_part_1(&prev, &curr, &next);
+        let lines: Vec<&str> = input.lines().collect();
+        let padding_line = &".".repeat(lines[0].len());
+        for i in 0..lines.len() {
+            let prev = format!(".{}.", if i == 0 { padding_line } else { lines[i - 1] });
+            let curr = format!(".{}.", lines[i]);
+            let next = format!(
+                ".{}.",
+                if i == lines.len() - 1 {
+                    padding_line
+                } else {
+                    lines[i + 1]
+                }
+            );
+            sum += check_line(&prev, &curr, &next);
+        }
+        sum
     }
-    sum
+
+    fn check_line(prev: &str, curr: &str, next: &str) -> u32 {
+        let numbers: Vec<&str> = curr
+            .split(|ch: char| !ch.is_numeric())
+            .filter(|x| x != &"")
+            .collect();
+        let mut sum = 0;
+        let mut start_index = 0;
+
+        for number in numbers {
+            let index = curr[start_index..].find(number).unwrap() + start_index;
+
+            let mut test_set = String::new();
+            let start = index - 1;
+            let end = index + number.len() + 1;
+            for line in [prev, curr, next] {
+                test_set.push_str(&line[start..end]);
+            }
+
+            if test_set.chars().any(|x| !x.is_numeric() && x != '.') {
+                sum += number.parse::<u32>().unwrap();
+            }
+            start_index = index + number.len();
+        }
+        sum
+    }
 }
 
-fn check_line_part_1(prev: &str, curr: &str, next: &str) -> u32 {
-    let numbers: Vec<&str> = curr
-        .split(|ch: char| !ch.is_numeric())
-        .filter(|x| x != &"")
-        .collect();
-    let mut sum = 0;
-    let mut start_index = 0;
+mod part_2 {
+    pub(crate) fn solve(input: &str) -> u32 {
+        // find `*` that is adjacent to exactly two numbers
+        // multiply numbers together
+        // sum all these together
+        let mut sum = 0;
 
-    for number in numbers {
-        let index = curr[start_index..].find(number).unwrap() + start_index;
+        let lines: Vec<&str> = input.lines().collect();
+        let padding_line = &".".repeat(lines[0].len());
+        for i in 0..lines.len() {
+            let prev = format!(".{}.", if i == 0 { padding_line } else { lines[i - 1] });
+            let curr = format!(".{}.", lines[i]);
+            let next = format!(
+                ".{}.",
+                if i == lines.len() - 1 {
+                    padding_line
+                } else {
+                    lines[i + 1]
+                }
+            );
+            sum += check_line(&prev, &curr, &next);
+        }
+        sum
+    }
 
-        let mut test_set = String::new();
-        let start = index - 1;
-        let end = index + number.len() + 1;
+    fn check_line(prev: &str, curr: &str, next: &str) -> u32 {
+        let star_indeces: Vec<(usize, &str)> = curr.match_indices('*').collect();
+        let mut sum = 0;
+
+        let mut numbers = vec![];
         for line in [prev, curr, next] {
-            test_set.push_str(&line[start..end]);
+            numbers.extend(find_number_indeces(line));
         }
 
-        if test_set.chars().any(|x| !x.is_numeric() && x != '.') {
-            sum += number.parse::<u32>().unwrap();
+        for (star_index, _) in star_indeces {
+            sum += find_prodcut(&numbers, star_index);
         }
-        start_index = index + number.len();
-    }
-    sum
-}
 
-fn solve_part_2(input: &str) -> u32 {
-    // find `*` that is adjacent to exactly two numbers
-    // multiply numbers together
-    // sum all these together
-    let mut sum = 0;
-
-    let lines: Vec<&str> = input.lines().collect();
-    let padding_line = &".".repeat(lines[0].len());
-    for i in 0..lines.len() {
-        let prev = format!(".{}.", if i == 0 { padding_line } else { lines[i - 1] });
-        let curr = format!(".{}.", lines[i]);
-        let next = format!(
-            ".{}.",
-            if i == lines.len() - 1 {
-                padding_line
-            } else {
-                lines[i + 1]
-            }
-        );
-        sum += check_line_part_2(&prev, &curr, &next);
-    }
-    sum
-}
-
-fn check_line_part_2(prev: &str, curr: &str, next: &str) -> u32 {
-    let star_indeces: Vec<(usize, &str)> = curr.match_indices('*').collect();
-    let mut sum = 0;
-
-    let mut numbers = vec![];
-    for line in [prev, curr, next] {
-        numbers.extend(find_number_indeces(line));
+        sum
     }
 
-    for (star_index, _) in star_indeces {
-        sum += find_prodcut(&numbers, star_index);
+    fn find_number_indeces(line: &str) -> Vec<(usize, &str)> {
+        let mut found = vec![];
+        let numbers = line.split(|ch: char| !ch.is_numeric()).filter(|x| x != &"");
+        let mut start_index = 0;
+
+        for number in numbers {
+            let index = line[start_index..].find(number).unwrap() + start_index;
+            found.push((index, number));
+            start_index = index + number.len();
+        }
+
+        found
     }
 
-    sum
-}
-
-fn find_number_indeces(line: &str) -> Vec<(usize, &str)> {
-    let mut found = vec![];
-    let numbers = line.split(|ch: char| !ch.is_numeric()).filter(|x| x != &"");
-    let mut start_index = 0;
-
-    for number in numbers {
-        let index = line[start_index..].find(number).unwrap() + start_index;
-        found.push((index, number));
-        start_index = index + number.len();
-    }
-
-    found
-}
-
-fn find_prodcut(numbers: &Vec<(usize, &str)>, star_index: usize) -> u32 {
-    let adjacent_nums: Vec<&(usize, &str)> = numbers
-        .iter()
-        .filter(|(i, num)| star_index <= i + num.len() && star_index >= i - 1)
-        .collect();
-
-    if adjacent_nums.len() == 2 {
-        return adjacent_nums
+    fn find_prodcut(numbers: &Vec<(usize, &str)>, star_index: usize) -> u32 {
+        let adjacent_nums: Vec<&(usize, &str)> = numbers
             .iter()
-            .map(|(_, num)| num.parse::<u32>().unwrap())
-            .product();
-    }
+            .filter(|(i, num)| star_index <= i + num.len() && star_index >= i - 1)
+            .collect();
 
-    0
+        if adjacent_nums.len() == 2 {
+            return adjacent_nums
+                .iter()
+                .map(|(_, num)| num.parse::<u32>().unwrap())
+                .product();
+        }
+
+        0
+    }
 }
 
 #[cfg(test)]
@@ -132,7 +136,7 @@ mod tests {
     #[test]
     fn example_input_part_1_test() {
         assert_eq!(
-            solve_part_1(
+            part_1::solve(
                 "467..114..\n\
             ...*......\n\
             ..35..633.\n\
@@ -151,7 +155,7 @@ mod tests {
     #[test]
     fn part_1_adjacent_numbers_test() {
         assert_eq!(
-            solve_part_1(
+            part_1::solve(
                 "467..114..\n\
                 .12*......\n\
                 ..35..633."
@@ -163,7 +167,7 @@ mod tests {
     #[test]
     fn part_1_negative_numbers_test() {
         assert_eq!(
-            solve_part_1(
+            part_1::solve(
                 "467..114..\n\
                 -12*......\n\
                 ..35..633."
@@ -175,7 +179,7 @@ mod tests {
     #[test]
     fn part_1_end_test() {
         assert_eq!(
-            solve_part_1(
+            part_1::solve(
                 "467..114..\n\
                 -12*.....-\n\
                 ..35...633"
@@ -187,7 +191,7 @@ mod tests {
     #[test]
     fn example_input_part_2_test() {
         assert_eq!(
-            solve_part_2(
+            part_2::solve(
                 "467..114..\n\
             ...*......\n\
             ..35..633.\n\
