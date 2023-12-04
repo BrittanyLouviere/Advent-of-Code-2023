8d3e2e69680b00fc0800eac6a26a56656d2b8ef1
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-04 20:59:07 +1300

    changed constant name

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -55,7 +55,7 @@ mod part_2 {
 mod tests {
     use super::*;
 
-    const example_input: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
+    const EXAMPLE_INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
     Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
     Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n\
     Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
@@ -64,11 +64,11 @@ mod tests {
 
     #[test]
     fn example_input_part_1_test() {
-        assert_eq!(part_1::solve(&example_input), 13);
+        assert_eq!(part_1::solve(&EXAMPLE_INPUT), 13);
     }
 
     #[test]
     fn example_input_part_2_test() {
-        assert_eq!(part_2::solve(&example_input), 30);
+        assert_eq!(part_2::solve(&EXAMPLE_INPUT), 30);
     }
 }
