61844b05814a4ba665aeedee9379a9f50bf9c7b3
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-09 07:13:49 +1300

    example setup

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -24,11 +24,26 @@ mod part_2 {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT: &str = "";
+    const EXAMPLE_INPUT: &str = r"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+    const EXAMPLE_INPUT_2: &str = r"LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
 
     #[test]
     fn example_input_part_1_test() {
-        assert_eq!(part_1::solve(EXAMPLE_INPUT), 0);
+        assert_eq!(part_1::solve(EXAMPLE_INPUT), 2);
+        assert_eq!(part_1::solve(EXAMPLE_INPUT_2), 6);
     }
 
     #[test]
