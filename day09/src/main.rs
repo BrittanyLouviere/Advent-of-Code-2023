45bde0610cd18ad70848545606d937d2b0f73769
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-16 08:06:01 +1300

    Part 1 setup

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -24,11 +24,13 @@ mod part_2 {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT: &str = "";
+    const EXAMPLE_INPUT: &str = r"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
     #[test]
     fn example_input_part_1_test() {
-        assert_eq!(part_1::solve(EXAMPLE_INPUT), 0);
+        assert_eq!(part_1::solve(EXAMPLE_INPUT), 114);
     }
 
     #[test]
