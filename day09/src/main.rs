d2f25c06442ac682e3cd964926d68491f51522b2
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2023-12-16 09:08:43 +1300

    Moved extrapolate() to utility module

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -8,20 +8,8 @@ fn main() {
     println!("Part 2: {}", part_2::solve(&input));
 }
 
-mod part_1 {
-    pub(crate) fn solve(input: &str) -> i32 {
-        let mut sum = 0;
-        for line in input.lines() {
-            let history = line
-                .split_whitespace()
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect();
-            sum += extrapolate(history);
-        }
-        sum
-    }
-
-    fn extrapolate(history: Vec<i32>) -> i32 {
+pub(crate) mod utility {
+    pub(crate) fn extrapolate(history: Vec<i32>, next_val: bool) -> i32 {
         let mut sequences = vec![history];
         loop {
             let current_seq = sequences.last().unwrap();
@@ -39,13 +27,19 @@ mod part_1 {
 
         let mut current_num = 0;
         for seq in sequences.iter().rev() {
-            current_num += seq.last().unwrap();
+            if next_val {
+                current_num += seq.last().unwrap();
+            } else {
+                current_num = seq.first().unwrap() - current_num;
+            }
         }
         current_num
     }
 }
 
-mod part_2 {
+mod part_1 {
+    use crate::utility::extrapolate;
+
     pub(crate) fn solve(input: &str) -> i32 {
         let mut sum = 0;
         for line in input.lines() {
@@ -53,32 +47,25 @@ mod part_2 {
                 .split_whitespace()
                 .map(|x| x.parse::<i32>().unwrap())
                 .collect();
-            sum += extrapolate(history);
+            sum += extrapolate(history, true);
         }
         sum
     }
+}
 
-    fn extrapolate(history: Vec<i32>) -> i32 {
-        let mut sequences = vec![history];
-        loop {
-            let current_seq = sequences.last().unwrap();
-            let mut new_seq = vec![];
-            for i in 0..(current_seq.len() - 1) {
-                let v1 = current_seq[i];
-                let v2 = current_seq[i + 1];
-                new_seq.push(v2 - v1);
-            }
-            if new_seq.iter().all(|x| x == &0) {
-                break;
-            }
-            sequences.push(new_seq);
-        }
+mod part_2 {
+    use crate::utility::extrapolate;
 
-        let mut current_num = 0;
-        for seq in sequences.iter().rev() {
-            current_num = seq.first().unwrap() - current_num;
+    pub(crate) fn solve(input: &str) -> i32 {
+        let mut sum = 0;
+        for line in input.lines() {
+            let history = line
+                .split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect();
+            sum += extrapolate(history, false);
         }
-        current_num
+        sum
     }
 }
 
